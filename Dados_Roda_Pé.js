33625efbac9c50fc657350fe71d2b57147043ca7
Kaiky Bitencourt <kaiky.developer@gmail.com>
2024-05-02 23:39:23 -0300

    Alguns retoques e adesão de javascript

---
+++ Dados_Roda_Pé.js
@@ -0,0 +1,10 @@
+var varNomeCompleto, varAnoCorrente, varTwitter, varFacebook, varInstagram, varLinkedln, varSexo, varPolo;
+
+varNomeCompleto =   "Kaiky Bitencourt";
+varAnoCorrente  =   "2024";
+varTwitter      =   "@kaikytt";
+varFacebook     =   "@kaiky.biten";
+varInstagram    =   "@kaikyy_";
+varLinkedln     =   "@kaiky.biten";
+varSexo         =   "Masculino";
+varPolo         =   "Higenópolis";
